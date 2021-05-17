cff437ea862359f64f772c1f68c9d589aa16b2e9
César Mejía <cesarmejiaguzman@gmail.com>
2021-05-17 18:46:25 -0500

    Our story done

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -14,4 +14,15 @@
             .catch(res => { console.log('Can not find service worker.') })
     }
 
+    const q = (selector, target) => (target || document).querySelector(selector);
+    const qa = (selector, target) => (target || document).querySelectorAll(selector);
+
+    const video = q('.video-ribbon video');
+
+    if (video) {
+        window.addEventListener('load', e => {
+            // video.play();
+        });
+    }
+
 })()
