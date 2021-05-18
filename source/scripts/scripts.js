3d7aeebb6a8b324cc1a8cbd2262adff868865c24
César Mejía <cesarmejiaguzman@gmail.com>
2021-05-18 17:37:59 -0500

    Navigation done

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -17,12 +17,40 @@
     const q = (selector, target) => (target || document).querySelector(selector);
     const qa = (selector, target) => (target || document).querySelectorAll(selector);
 
-    const video = q('.video-ribbon video');
 
-    if (video) {
-        window.addEventListener('load', e => {
-            // video.play();
-        });
+    // Globals
+    const navigation = q('.navigation');
+
+    function changeNavColor() {
+        if (window.scrollY > 100) {
+            navigation.classList.add('inverted-color');
+        } else {
+            navigation.classList.remove('inverted-color');
+        }
     }
+
+    function expandMenu() {
+        const list = q('.list-wrapper', navigation);
+        list.classList.toggle('list-expanded');
+        navigation.classList.toggle('list-visible');
+    }
+    
+    // Navigation
+    (() => {
+        const toggleBtn = q('.toggle-btn', navigation);
+        toggleBtn.addEventListener('click', expandMenu);
+
+        changeNavColor();
+    })();
+    
+
+    // Events
+    window.addEventListener('resize', e => {
+        changeNavColor();
+    });
+
+    window.addEventListener('scroll', e => {
+        changeNavColor();
+    });
 
 })()
